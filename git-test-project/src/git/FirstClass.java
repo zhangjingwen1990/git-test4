65374cf4e43129cbc8b6748bac48bb03f045dd99
zhang <zhang@itcast.cn>
2020-08-21 01:09:59 +0800

    添加本地仓库

---
+++ git-test-project/src/git/FirstClass.java
@@ -0,0 +1,14 @@
+package git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        //hello test
+    }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
+}
